9a3257f121e887abf4b5569d32e1c6ea9b06101f
mattvh89 <matt.van.helden@hotmail.com>
2025-06-08 16:32:36 -0400

    Update Account.h

--- Account.h
+++ Account.h
@@ -5,52 +5,52 @@ class Account
 {
 public:
 // Constructors
-																Account								();
+													Account								();
 
-																Account								(const char* fname,
-																									 const char* pw);
+													Account								(const char* fname,
+																					 const char* pw);
 
-							Account&							operator=							(const Account& a);
+						Account&						operator=							(const Account& a);
 
-							Account&							operator=							(Account&& a)									noexcept;
+						Account&						operator=							(Account&& a)									noexcept;
 
-							//Account&							operator+							(const Account& a);
+							//Account&					operator+							(const Account& a);
 
 // Getters
 	inline					std::string_view					fileName							()										const					{ return m_fileName; }
 
 	inline					std::string_view					accountName							()										const					{ return m_name;     }
 
-	inline					bool								isGood								()										const					{ return m_good;     }
+	inline					bool							isGood								()										const					{ return m_good;     }
 
-	inline					double								getBalance							()										const					{ return m_total;    }
+	inline					double							getBalance							()										const					{ return m_total;    }
 
-	inline		const		Ledger&								getLedger							()										const					{ return m_ledger;   }
+	inline		const			Ledger&							getLedger							()										const					{ return m_ledger;   }
 
-	inline					Ledger&								getLedger							()																{ return m_ledger;   }
+	inline					Ledger&							getLedger							()																{ return m_ledger;   }
 
 
 
 // Setters
 									/* add and remove transaction will also update the corresponding file */
-							size_t								addTransaction						(const Transaction& t);
+						size_t							addTransaction							(const Transaction& t);
 
-							size_t								removeLastTransaction				();
+						size_t							removeLastTransaction						();
 
-							size_t								removeTransaction					(const size_t& index);
+						size_t							removeTransaction						(const size_t& index);
 
-							size_t								removeTransaction					(const Transaction& t);
+						size_t							removeTransaction						(const Transaction& t);
 
-							double								calculateBalance					();
+						double							calculateBalance						();
 
-	inline					void								setName								(std::string_view name)											{ m_name = name;}
-
+	inline					void							setName								(std::string_view name)											{ m_name = name;}
+	
 private:
 	Ledger      m_ledger;
 	std::string m_name,
-				m_fileName,
-				m_pw;
+		    m_fileName,
+		    m_pw;
 	double      m_total;
-	bool		m_good;
+	bool	    m_good;
 };
 
